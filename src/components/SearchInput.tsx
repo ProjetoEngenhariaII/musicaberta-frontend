ce8e1e1a550e7d4b5e8f616769b656bd596eaf61
gian <gian@pop-os.localdomain>
2024-10-17 11:15:33 -0300

    search input component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { ChangeEvent } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function SearchInput() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleChange = useDebouncedCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const searchString = e.target.value;
+      const params = new URLSearchParams(searchParams);
+
+      if (searchString) {
+        params.set("search", searchString);
+        params.set("page", "1");
+      } else {
+        params.delete("search");
+      }
+
+      replace(`${pathname}?${params.toString()}`);
+    },
+    700
+  );
+
+  return (
+    <div className="relative">
+      <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+      <Input
+        type="search"
+        placeholder="Buscar partituras..."
+        className="w-full rounded-lg bg-background pl-8"
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
