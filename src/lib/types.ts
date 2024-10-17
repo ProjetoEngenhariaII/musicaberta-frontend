7599a8ade7b13a2840a6addf2069f342b56c98e7
gian <gian@pop-os.localdomain>
2024-10-17 11:16:51 -0300

    add types

---
+++ src/lib/types.ts
@@ -0,0 +1,25 @@
+export type Sheet = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  title: string;
+  songWriter: string;
+  pdfUrl: string;
+  mp3Url: string;
+  badges: string[];
+  userId: string;
+};
+
+export type MetaResponseBody = {
+  current: number;
+  path: string;
+  prev: number | null;
+  next: number | null;
+  last: number;
+  total: number;
+};
+
+export type SheetsResponseBody = {
+  data: Sheet[];
+  meta: MetaResponseBody;
+};
