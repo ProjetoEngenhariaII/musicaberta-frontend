ef55e48fd150b272544f7819ed1ef21f5dc36016
gian <gian@pop-os.localdomain>
2024-10-17 11:14:27 -0300

    filter dropdown component

---
+++ src/components/FilterDropDown.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import { Filter } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export function FilterDropDown() {
+  const [filter, setFilter] = useState("desc");
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleChangeFilter(value: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set("sort", value);
+    } else {
+      params.delete("sort");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+    setFilter(value);
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-1 text-slate-600">
+          <Filter className="w-4 h-4" />
+          Filtros
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Filtrar por:</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup
+          value={filter}
+          onValueChange={handleChangeFilter}
+        >
+          <DropdownMenuRadioItem value="desc">
+            Mais recentes
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="asc">
+            Mais antigos
+          </DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
