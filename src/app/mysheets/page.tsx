58d65686f3473c8466ed3165799bf9d981108579
gian <gian@pop-os.localdomain>
2024-10-18 18:10:23 -0300

    update mysheets page

--- src/app/mysheets/page.tsx
+++ src/app/mysheets/page.tsx
@@ -1,13 +1,26 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { api } from "@/lib/axios";
+import { Sheet } from "@/lib/types";
+import SheetCardContainer from "@/components/SheetCardContainer";
 
 export default async function MySheets() {
   const session = await getServerSession(authOptions);
 
-  if (session) {
-    return <h1>Gerenciar minhas partituras</h1>;
+  if (!session) {
+    redirect("/login");
   }
 
-  redirect("/login");
+  const userId = session.user.id;
+  const res = await api.get(`/sheets/user/${userId}`);
+
+  const sheets: Sheet[] = res.data.sheets;
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-12 py-8 px-4">
+      <h1 className="text-3xl font-bold">Minhas partituras</h1>
+      <SheetCardContainer sheets={sheets} />
+    </div>
+  );
 }
