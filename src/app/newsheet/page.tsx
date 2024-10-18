e623e745c13386173ffd7012e0da5392689a856d
gian <gian@pop-os.localdomain>
2024-10-18 18:08:06 -0300

    create page new sheet

---
+++ src/app/newsheet/page.tsx
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import NewSheetForm from "@/components/NewSheetForm";
+
+export default async function NewSheet() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  const userId = session.user.id;
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-12 py-8 px-4">
+      <h1 className="text-3xl font-bold">Upload de partitura</h1>
+
+      <NewSheetForm userId={userId} />
+    </div>
+  );
+}
