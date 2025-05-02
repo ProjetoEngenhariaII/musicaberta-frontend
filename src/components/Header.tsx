53a56641bee39b2859a404e2a9e7d8cf82e1b835
gian <lgian584@gmail.com>
2025-05-02 16:48:44 -0300

    feat: add link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export default async function Header() {
 
   return (
     <header className="flex justify-between items-center w-full max-w-screen-xl p-3 mx-auto sticky top-0 z-50 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="flex justify-center items-center gap-2 ">
+      <Link href="/" className="flex justify-center items-center gap-2 ">
         <Avatar>
           <AvatarImage
             className="size-12 rounded-full"
@@ -36,7 +36,7 @@ export default async function Header() {
         <h1 className="hidden min-[500px]:block text-2xl text-slate-950 font-semibold">
           Músicaberta
         </h1>
-      </div>
+      </Link>
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
