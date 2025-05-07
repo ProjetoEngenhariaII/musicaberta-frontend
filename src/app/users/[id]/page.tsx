366a6540cab839fef55357b0f73b96103559c383
gian <lgian584@gmail.com>
2025-05-07 19:48:12 -0300

    feat: user page initial

---
+++ src/app/users/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function RequestDetails({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  return <h1>user: {id}</h1>;
+}
