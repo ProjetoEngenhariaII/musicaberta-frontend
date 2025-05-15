153450e04d0a7414a531de4b7103b6cc76c7819a
gian <lgian584@gmail.com>
2025-05-15 16:12:22 -0300

    feat: add loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center mt-36">
+      <h1 className="text-3xl font-bold">Carregando...</h1>
+    </div>
+  );
+}
